7f0229db4180a57ac24b35fe137a47ee1cb70f4d
Hung <tranhung6829@gmail.com>
2026-05-14 11:21:27 +0700

    ud

--- src/pages/GiaoXuDetail.jsx
+++ src/pages/GiaoXuDetail.jsx
@@ -326,6 +326,7 @@ const GiaoXuDongQuanFull = () => {
                                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.9286361938503!2d106.4010962751596!3d20.42041368108102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135fb75aaaaaaab%3A0xfb0f0731b568e408!2zTmjDoCBUaOG7nSBHacOhbyBY4bupIMSQ4buTbmcgUXVhbg!5e1!3m2!1svi!2s!4v1778647717285!5m2!1svi!2s" 
                                         width="100%" height="100%" style={{ border: 0 }} 
                                         allowFullScreen={true} loading="lazy" 
+                                        title="Bản đồ giáo xứ"
                                     />
                                 </div>
                                 <div className="map-footer">
